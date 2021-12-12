de73d0568a490c0fe1df5bb6c90cb5a2d0aae866
PavloLevchenko <teasetrnet@gmail.com>
2021-12-12 08:05:39 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,31 +4,24 @@ form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
 	event.preventDefault();
 	const inputs = event.currentTarget.elements; //требование дз
+	const fields = checkForm(inputs);
+	if (fields != null) {
+		console.log(fields);
+		event.currentTarget.reset();
+	}
+}
+
+function checkForm(inputs) {
 	const fields = {};
 	for (const input of inputs) {
 		if (input.type != "submit") {
 			if (input.value == "") {
 				alert("All fields must be filled!");
+				return null;
 			} else {
 				fields[input.name] = input.value;
 			}
 		}
 	}
-	console.log(fields);
-	event.currentTarget.reset();
+	return fields;
 }
-
-/* function handleSubmit(event) {
-	event.preventDefault();
-	const inputs = event.currentTarget.querySelectorAll("input");
-	const fields = {};
-	for (const input of inputs) {
-		if (input.value == "") {
-			alert("All fields must be filled!");
-		} else {
-			fields[input.name] = input.value;
-		}
-	}
-	console.log(fields);
-	event.currentTarget.reset();
-} */
